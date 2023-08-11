e086bb75a851ff3e30d8becc7b95ee9eb8b35a35
ROBERDAN DE ANDRADE CORREIA <roberdan.acorreia@senacsp.edu.br>
2023-08-11 17:18:16 -0300

    Iniciando HomePage

---
+++ BakeryStock/Pages/CreateAccount.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BakeryStock.Pages
+{
+    public class CreateAccountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
