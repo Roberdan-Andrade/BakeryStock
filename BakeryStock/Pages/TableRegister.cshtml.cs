5a7d9599243bc6acbc80c2db51b1120091cf7dd9
ROBERDAN DE ANDRADE CORREIA <roberdan.acorreia@senacsp.edu.br>
2023-08-21 16:42:47 -0300

    Finalização do front-end

---
+++ BakeryStock/Pages/TableRegister.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BakeryStock.Pages
+{
+    public class TableRegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
